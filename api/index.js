5d571473049b20fd43a3e5bd5098445a06085dee
ecru <woovoodoor@yahoo.co.jp>
2020-12-23 08:02:45 +0900

    修正

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var nodemon = require('nodemon');
 
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
@@ -10,12 +11,13 @@ var connection = mysql.createConnection({
 });
 
 app.get('/', function (req, res) {
+  res.set({ 'Access-Control-Allow-Origin': '*' });
   connection.query('select * from users', function (error, results, fields) {
     if (error) throw error;
     res.send(results[0]);
   });
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(5000, function () {
+  console.log('Example app listening on port 5000!');
 });
