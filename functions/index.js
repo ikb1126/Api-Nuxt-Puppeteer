8109fd54cff80152a627baeeadef438e5b553f86
ecru <woovoodoor@yahoo.co.jp>
2020-12-24 08:02:53 +0900

    作成

---
+++ functions/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+const mysql      = require('mysql');
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'kumakuma',
+  database : 'database_development'
+});
+
+app.get('/', function (req, res) {
+  //res.set({ 'Access-Control-Allow-Origin': '*' });
+  connection.query('select * from users', function (error, results, fields) {
+    if (error) throw error;
+    res.send(results[0]);
+  });
+});
+
+app.listen(5000, function () {
+  console.log('Example app listening on port 5000!');
+});
